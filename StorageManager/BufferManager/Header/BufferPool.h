5390bc39272e1e878e8d339025652ad595bed02f
Michael Ticona <mjticonala@gmail.com>
2024-06-10 00:06:56 -0500

    se arreglaron las funciones de BufferPool

--- StorageManager/BufferManager/Header/BufferPool.h
+++ StorageManager/BufferManager/Header/BufferPool.h
@@ -4,56 +4,11 @@
 
 class BufferPool {
 private:
-    std::vector<Frame> frames;
-    //int lastUsedCounter;  // Contador para el seguimiento de �ltimo uso de los frames
-
+    std::vector<Frame> BufferFrame;
 public:
-    BufferPool(int size);
-    /*
-    Constructor de la clase BufferPool. Toma el tamaño del conjunto de marcos como argumento e inicializa el 
-    conjunto de marcos con la cantidad especificada de marcos. No solo eso, también inicializa el contador de 
-    "último uso" de cada marco a 0.
-    */
-    void updateCount(int frameID);
-    /*
-    Actualizar el pin count de un frame en especifico
-    */
-    void setPage(int pageID, int frameID);
-    /*
-    Esta función establece una página en el marco especificado por su ID (frameID). Verifica si el frameID es válido 
-    dentro del rango de marcos disponibles. Si es así, asigna el ID de la página especificada al marco correspondiente 
-    y restablece el indicador de suciedad (dirtyFlag) del marco. Luego, actualiza el contador de "último uso" del marco.
-    */
-    Frame& getFrame(int frameID);
-    /*
-    Esta función devuelve una referencia al marco especificado por su ID (frameID). Antes de devolver el marco, 
-    actualiza su contador de "último uso".
-    */
-    void releaseFrame(int frameID);
-    /*
-    Esta función libera el marco especificado por su ID (frameID). Al "liberar" un marco, se asigna un marco vacío 
-    a ese ID de marco, restableciendo así todos sus valores.
-    */
-    int size();
-    /*
-    Esta función devuelve el tamaño del conjunto de marcos, es decir, la cantidad total de marcos en el conjunto.
-    */
-    int getLeastRecentlyUsed();
-    /*
-    Esta función busca y devuelve el índice del marco menos recientemente utilizado en el conjunto. Itera sobre todos 
-    los marcos disponibles, excluyendo aquellos que están "pinneados" (bloqueados), y devuelve el índice del marco 
-    con el menor valor de "último uso".
-    */
-    void updateLastUsed(int frameID);
-    /*
-    Esta función actualiza el contador de "último uso" del marco especificado por su ID (frameID). Sin embargo, solo 
-    actualiza el contador si el marco no está "pinneado" (bloqueado).
-    */
-    int getLastUsedCounter();  // Devuelve el contador de �ltimo uso
-    /*
-    Esta función devuelve el valor mínimo del contador de "último uso" entre todos los marcos. Se utiliza para determinar 
-    qué marco ha sido el menos recientemente utilizado. Sin embargo, parece que hay un error en esta función, ya que 
-    debería verificar si los marcos están "pinneados" antes de considerar su "último uso".
-    */
-    void increment(int);
+    BufferPool(int, int);
+    Frame* GetFrame(int frameId);
+    void PinFrame(int frameId);
+    void UnpinFrame(int frameId);
+    void ResetBufferPool();
 };
--- StorageManager/BufferManager/Sources/BufferPool.cpp
+++ StorageManager/BufferManager/Sources/BufferPool.cpp
@@ -1,67 +1,37 @@
 #include "BufferPool.h"
+#include <iostream>
 
-BufferPool::BufferPool(int size) : frames(size) {
-    // Inicializa el buffer pool con un tama�o fijo y el contador de �ltimo uso a 0.
-}
-
-void BufferPool::updateCount(int frameID) {
-    if (frameID < frames.size()) {
-        frames[frameID].pin();  // Incrementa y asigna el nuevo valor de último uso.
-    }
-}
-void BufferPool::setPage(int pageID, int frameID) {
-    if (frameID < frames.size()) {
-        frames[frameID].pageID = pageID;
-        frames[frameID].dirtyFlag = false;  // Reinicia el flag de dirty
-        updateLastUsed(frameID);  // Actualiza el contador de �ltimo uso
+BufferPool::BufferPool(int size, int CapacityFrame) {
+    bufferFrames.reserve(size);
+    for (int i = 0; i < size; ++i) {
+        bufferFrames.emplace_back(Frame(CapacityFrame)); // o con el tamaño apropiado
     }
 }
 
-Frame& BufferPool::getFrame(int frameID) {
-    //updateLastUsed(frameID);  // Actualiza cada vez que se accede a un frame
-    return frames[frameID];
-}
-
-void BufferPool::releaseFrame(int frameID) {
-    frames[frameID] = Frame();
-}
-
-int BufferPool::size() {
-    return frames.size();
-}
-
-int BufferPool::getLeastRecentlyUsed() {
-    int lruIndex = -1;
-    int minLastUsed = INT_MAX;  // Inicializa con el m�ximo valor posible
-    for (int i = 0; i < frames.size(); ++i) {
-        if (frames[i].pinCount == 0 && frames[i].lastUsed < minLastUsed) {
-            minLastUsed = frames[i].lastUsed;
-            lruIndex = i;
-        }
+Frame* BufferPool::GetFrame(int frameID) {
+    if (frameID >= 0 && frameID < bufferFrames.size()) {
+        return &bufferFrames[frameID];
     }
-    return lruIndex;  // Devuelve el �ndice del frame menos recientemente usado
+    std::cout<<"No se encontro el Frame"<<std::endl;
+    return nullptr;
 }
 
-void BufferPool::updateLastUsed(int frameID) {
-    if (frameID < frames.size() /*&& !frames[frameID].isPinned*/) { // Solo actualiza si el frame no est� pinned
-        frames[frameID].lastUsed++;
+void BufferPool::PinFrame(int frameID) {
+    Frame* frame = GetFrame(frameID);
+    if (frame) {
+        frame->pin();
     }
 }
-    
-int BufferPool::getLastUsedCounter() { // falta comprobar que sea 0 isPinned tmbn
-    int minLastUsed = frames[0].lastUsed;
-    for (int i = 1; i < frames.size(); ++i) { 
-        if (frames[i].lastUsed < minLastUsed) {
-            minLastUsed = frames[i].lastUsed; 
-        }
+
+void BufferPool::UnpinFrame(int frameID) {
+    Frame* frame = GetFrame(frameID);
+    if (frame) {
+        frame->unpin();
     }
-    return minLastUsed; 
 }
 
-void BufferPool::increment(int frameID)
-{
-    for(int i = 0; i < frames.size(); i++){
-        if(i != frameID && frames[i].pinCount)
-            frames[i].lastUsed++;
+void BufferPool::ResetBufferPool() {
+    for (auto& frame : bufferFrames) {
+        frame.reset();
     }
 }
