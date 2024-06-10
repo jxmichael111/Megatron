82a79645bdcce0351c73e1011f82bf5eced31f62
Michael Ticona <mjticonala@gmail.com>
2024-06-10 00:34:30 -0500

    se cambio la funciones de PageTable

--- StorageManager/BufferManager/Header/PageTable.h
+++ StorageManager/BufferManager/Header/PageTable.h
@@ -1,26 +1,12 @@
 #pragma once
-#include <map>
+#include <unordered_map>
 
 class PageTable {
 public:
-    std::map<int, int> pageMap;
-
-    int getFrame(int pageID);
-    /*
-    Esta función busca en la tabla de páginas (pageMap) el ID de marco asociado con un ID de página dado (pageID).
-    Si encuentra una entrada correspondiente en el mapa, devuelve el ID de marco asociado. Si no encuentra ninguna 
-    entrada para el ID de página dado, devuelve -1 para indicar que la página no está presente en la tabla de páginas.
-    */
-    void setFrame(int pageID, int frameID);
-    /*
-    Esta función establece o actualiza la asociación entre un ID de página y un ID de marco en la tabla de 
-    páginas (pageMap). Toma el ID de página (pageID) y el ID de marco (frameID) como argumentos y los asigna 
-    en el mapa de páginas.
-    */
-    void removePage(int pageID);
-    /*
-    Esta función elimina una entrada específica de la tabla de páginas. Toma el ID de página (pageID) como
-    argumento y elimina la entrada correspondiente en el mapa de páginas. Si no hay ninguna entrada para 
-    el ID de página dado, la función no realiza ninguna acción.
-    */
+    std::unordered_map<int, int> pageMap;
+    PageTable();
+    void MapPageToFrame(int pageId, int frameId);
+    void UnmapPage(int pageId);
+    bool IsPageMapped(int pageId) const;
+    void ResetPageTable();
 };
--- StorageManager/BufferManager/Sources/PageTable.cpp
+++ StorageManager/BufferManager/Sources/PageTable.cpp
@@ -1,19 +1,22 @@
 #include "PageTable.h"
+#include <iostream>
 
-int PageTable::getFrame(int pageID) {
-    // Devuelve el Frame ID asociado con el Page ID dado, o -1 si no se encuentra.
-    auto it = pageMap.find(pageID);
-    if (it != pageMap.end()) {
-        return it->second;
-    } // lineups
-    return -1;  // Retorna -1 si la página no está presente
+// Constructor por defecto: no es necesario inicializar nada adicional en este caso
+PageTable::PageTable() {
 }
 
-void PageTable::setFrame(int pageID, int frameID) {
-    // Asocia un Page ID con un Frame ID en la tabla de páginas.
-    pageMap[pageID] = frameID;  // Establece o actualiza el mapeo
+void PageTable::MapPageToFrame(int pageID, int frameId) {
+    pageMap[pageID] = frameId;
 }
 
-void PageTable::removePage(int pageID) {
+void PageTable::UnmapPage(int pageID) {
     pageMap.erase(pageID);
+}
+
+bool PageTable::IsPageMapped(int pageID) const {
+    return pageMap.find(pageID) != pageMap.end();
+}
+
+void PageTable::ResetPageTable() {
+    pageMap.clear();
 }
